fbf143590167fd44d3f0b179f9f0cef47a7520a4
Atreyu Falkor <socialistrevolution@gmail.com>
2024-10-18 10:27:03 +0000

    feat: save score

--- number_guess.sh
+++ number_guess.sh
@@ -13,14 +13,18 @@ MAIN_MENU()
   fi
 
   #CHECK IF USER EXISTS
-  RESULT_USER=$($PSQL "SELECT name FROM users WHERE UPPER(name) = UPPER('$USER_NAME')")
+  RESULT_USER=$($PSQL "SELECT user_id, games_played, best_game FROM users WHERE UPPER(name) = UPPER('$USER_NAME')")
 
   if ! [[ -z $RESULT_USER ]]
   then
-    echo "Welcome back, $USER_NAME! You have played <games_played> games, and your best game took <best_game> guesses."
+    IFS='|' read -r -a ARR_USER <<< "$RESULT_USER"
+    echo "Welcome back, $USER_NAME! You have played ${ARR_USER[1]} games, and your best game took ${ARR_USER[2]} guesses."
+    USER_ID=${ARR_USER[0]}
   else
     echo "Welcome, $USER_NAME! It looks like this is your first time here."
-    RESULT_INSERT_USER=$($PSQL "INSERT INTO users (name) VALUES ('$USER_NAME')")
+    RESULT_INSERT_USER=$($PSQL "INSERT INTO users (name) VALUES ('$USER_NAME') RETURNING user_id")
+    IFS=' ' read -r -a RESULT_INSERT_USER <<< "$RESULT_INSERT_USER"
+    USER_ID=${RESULT_INSERT_USER[0]}
   fi
 
   THE_NUMBER=$RANDOM
@@ -38,9 +42,9 @@ MAIN_MENU()
         else
           if [[ $USER_NUMBER -lt $THE_NUMBER ]]
             then
-              echo "It's higher than that, guess again: ($THE_NUMBER)"
+              echo "It's higher than that, guess again:"
             else
-              echo "It's lower than that, guess again: ($THE_NUMBER)"
+              echo "It's lower than that, guess again:"
           fi
       fi
       read USER_NUMBER
@@ -50,6 +54,18 @@ MAIN_MENU()
 
     echo "You guessed it in $COUNTER_GUESSES tries. The secret number was $THE_NUMBER. Nice job!"
 
+    ((ARR_USER[1]++))
+
+    if [[ -z ${ARR_USER[2]} ]] 
+      then
+        ARR_USER[2]=$COUNTER_GUESSES
+      else
+      if [[ $COUNTER_GUESSES -lt ${ARR_USER[2]} ]]
+        then
+          ARR_USER[2]=$COUNTER_GUESSES
+      fi
+    fi
+    RESULT_UPDATE_USER=$($PSQL "UPDATE users SET games_played=${ARR_USER[1]}, best_game=${ARR_USER[2]} WHERE user_id=$USER_ID")
 }
 
 MAIN_MENU
