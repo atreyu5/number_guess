b595e7dfc7197f81102d3782742b7052829295ec
Atreyu Falkor <socialistrevolution@gmail.com>
2024-10-18 09:41:29 +0000

    feat: enforce integers only

--- number_guess.sh
+++ number_guess.sh
@@ -2,9 +2,7 @@
 PSQL="psql --username=freecodecamp --dbname=number_guess -t --no-align -c"
 
 MAIN_MENU()
-{
-  #THE_NUMBER = $RANDOM
-  
+{  
   echo "Enter your username:"
   read USER_NAME
   
@@ -15,15 +13,37 @@ MAIN_MENU()
   fi
 
   #CHECK IF USER EXISTS
-  RESULT_USER=$($PSQL "SELECT name FROM users WHERE name = '$USER_NAME'")
+  RESULT_USER=$($PSQL "SELECT name FROM users WHERE UPPER(name) = UPPER('$USER_NAME')")
 
   if ! [[ -z $RESULT_USER ]]
   then
     echo "Welcome back, $USER_NAME! You have played <games_played> games, and your best game took <best_game> guesses."
   else
-    echo "Couldn't find you in the database."
+    echo "Welcome, $USER_NAME! It looks like this is your first time here."
+    RESULT_INSERT_USER=$($PSQL "INSERT INTO users (name) VALUES ('$USER_NAME')")
   fi
 
+  read THE_NUMBER <<< "$RANDOM"
+
+  echo "Guess the secret number between 1 and 1000:"
+  
+  read USER_NUMBER
+
+  while [ $USER_NUMBER != $THE_NUMBER ]
+    do
+      if ! [[ "$USER_NUMBER" =~ ^[0-9]+$ ]]
+        then
+          echo "That is not an integer, guess again:"
+        else
+          if [[ $USER_NUMBER -lt $THE_NUMBER ]]
+            then
+              echo "It's lower than that, guess again: ($THE_NUMBER)"
+            else
+              echo "It's higher than that, guess again: ($THE_NUMBER)"
+          fi
+      fi
+      read USER_NUMBER
+    done
 
 }
 
